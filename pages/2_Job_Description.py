c1173bc930c2a0d90949bef2af9331e468f73c13
Samapti-Sahoo <sahoosamapti97@gmail.com>
2026-02-24 20:27:49 +0530

    Job description ultimate UI

--- pages/2_Job_Description.py
+++ pages/2_Job_Description.py
@@ -1,15 +1,46 @@
 import streamlit as st
 from utils import clean_text, extract_skills
 
-st.title("📝 Job Description")
+st.markdown('<div class="main-title">Job Description</div>',unsafe_allow_html=True)
 
-job_text = st.text_area("Paste Job Description")
+st.markdown("<br>",unsafe_allow_html=True)
+
+
+# Glass Card
+
+st.markdown('<div class="glass">',unsafe_allow_html=True)
+
+st.subheader("📄 Paste Job Description")
+
+job_text=st.text_area(
+
+"",
+height=250,
+
+placeholder="Paste job description here..."
+
+)
+
+st.markdown('</div>',unsafe_allow_html=True)
+
 
 if job_text:
-    cleaned_job = clean_text(job_text)
-    st.session_state.job_text = cleaned_job
 
-    job_skills = extract_skills(cleaned_job)
+ cleaned=clean_text(job_text)
 
-    st.subheader("Required Skills Detected")
-    st.write(job_skills)
+ st.session_state.job_text=cleaned
+
+
+ skills=extract_skills(cleaned)
+
+
+ st.markdown("<br>",unsafe_allow_html=True)
+
+
+ st.markdown('<div class="glass">',unsafe_allow_html=True)
+
+ st.subheader("🎯 Required Skills Detected")
+
+ st.success(skills)
+
+ st.markdown('</div>',unsafe_allow_html=True)
