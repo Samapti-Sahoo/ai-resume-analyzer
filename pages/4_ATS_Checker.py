7188d4744ce41889d26384c9b5e2766025e62780
Samapti-Sahoo <sahoosamapti97@gmail.com>
2026-02-24 20:29:29 +0530

    ATS Premium UI

--- pages/4_ATS_Checker.py
+++ pages/4_ATS_Checker.py
@@ -1,22 +1,75 @@
 import streamlit as st
+from scoring import calculate_similarity
+from utils import extract_skills
 
-st.title("🤖 ATS Compatibility Checker")
+st.markdown('<div class="main-title">ATS Checker</div>',unsafe_allow_html=True)
 
-if "resume_text" in st.session_state:
+st.markdown("<br>",unsafe_allow_html=True)
 
-    resume = st.session_state.resume_text
+if "resume_text" not in st.session_state or "job_text" not in st.session_state:
 
-    section_score = 100 if "education" in resume and "experience" in resume else 70
-    keyword_density = min(len(resume.split()) / 500, 1) * 100
+    st.markdown('<div class="glass">',unsafe_allow_html=True)
 
-    ats_score = (section_score + keyword_density) / 2
+    st.warning("Upload Resume and Job Description First")
 
-    st.metric("ATS Score", f"{round(ats_score,2)}%")
+    st.markdown('</div>',unsafe_allow_html=True)
 
-    if ats_score < 70:
-        st.warning("Improve keyword density and add proper sections.")
-    else:
-        st.success("Your resume is ATS friendly!")
+    st.stop()
 
-else:
-    st.warning("Upload resume first.")
+
+
+resume=st.session_state.resume_text
+job=st.session_state.job_text
+
+
+similarity=calculate_similarity(resume,job)
+
+
+resume_skills=extract_skills(resume)
+job_skills=extract_skills(job)
+
+
+matched=list(set(resume_skills)&set(job_skills))
+
+missing=list(set(job_skills)-set(resume_skills))
+
+
+ats_score=int(similarity*100)
+
+
+st.markdown('<div class="glass">',unsafe_allow_html=True)
+
+st.subheader("ATS Score")
+
+st.metric("Match Percentage",f"{ats_score}%")
+
+st.progress(ats_score)
+
+st.markdown('</div>',unsafe_allow_html=True)
+
+
+st.markdown("<br>",unsafe_allow_html=True)
+
+
+col1,col2=st.columns(2)
+
+with col1:
+
+ st.markdown('<div class="glass">',unsafe_allow_html=True)
+
+ st.subheader("Matched Skills")
+
+ st.success(matched)
+
+ st.markdown('</div>',unsafe_allow_html=True)
+
+
+with col2:
+
+ st.markdown('<div class="glass">',unsafe_allow_html=True)
+
+ st.subheader("Missing Skills")
+
+ st.error(missing)
+
+ st.markdown('</div>',unsafe_allow_html=True)
